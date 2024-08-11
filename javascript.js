ff32a9eb68ad1adce1b8f1f7c149ef67b050d033
Awardz <91509219+Awardz@users.noreply.github.com>
2024-08-11 15:24:01 -0400

    Finished the last step by adding the function playRound and score variables into the new playGame function.  Also moved the const functions into the for loop in the playGame function.  Personally I believe having the two functions seperate would make the code presentable and easier to follow.

--- javascript.js
+++ javascript.js
@@ -16,65 +16,90 @@ function getHumanChoice()
     return player.toLowerCase();
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(humanChoice, computerChoice)
+
+
+function playGame()
 {
-    if(humanChoice === "rock")
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(humanChoice, computerChoice)
     {
-        if(computerChoice === "Paper")
-            {
-                console.log("You Lose!  Paper Wins")
-                computerScore++;
-            }  
-        if(computerChoice === "Scissors")
-            {
-                console.log("You Win! Rock Beats Scissors");
-                humanScore++;
-            }
-        else
-            console.log("Draw!")
-    }
-
-    if(humanChoice === "paper")
+        if(humanChoice === "rock")
         {
-            if(computerChoice === "Scissors")
+            if(computerChoice === "Paper")
                 {
-                    console.log("You Lose! Scissors Wins!");
+                    console.log("You Lose!")
                     computerScore++;
-                } 
-            if(computerChoice === "Rock")
+                }  
+            else if(computerChoice === "Scissors")
                 {
-                    console.log("You Win!  Paper Wins!");
-                    humanScore++
+                    console.log("You Win!");
+                    humanScore++;
                 }
             else
                 console.log("Draw!")
         }
-
-        if(humanChoice === "scissors")
+    
+        if(humanChoice === "paper")
             {
-                if(computerChoice === "Rock")
+                if(computerChoice === "Scissors")
                     {
-                        console.log("You Lose! Rock Wins!");
+                        console.log("You Lose!");
                         computerScore++;
-                    }          
-                if(computerChoice === "Paper")
-                    {
-                        console.log("You Win! Scissors Wins!")
-                        humanScore++;
                     } 
+                else if(computerChoice === "Rock")
+                    {
+                        console.log("You Win!");
+                        humanScore++
+                    }
                 else
                     console.log("Draw!")
             }
+            if(humanChoice === "scissors")
+                {
+                    if(computerChoice === "Rock")
+                        {
+                            console.log("You Lose!");
+                            computerScore++;
+                        }          
+                    else if(computerChoice === "Paper")
+                        {
+                            console.log("You Win!")
+                            humanScore++;
+                        } 
+                    else
+                        console.log("Draw!")
+                } 
+                return console.log("On To The Next One!");
+    }
+
+    for(let i = 0; i < 5; i++)
+    {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        console.log("You Chose: " + humanSelection);
+        console.log("Computer Chose: " + computerSelection);
+        playRound(humanSelection, computerSelection);
+    }
+    
+    console.log("Player: " + humanScore);
+    console.log("Computer: " + computerScore);
+
+    if(humanScore > computerScore)
+        return console.log("Player Wins");
+    else if(humanScore < computerScore)
+        return console.log("Computer Wins");
+    else
+        return console.log("Draw");
 
     
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+
+//const humanSelection = getHumanChoice();
+//const computerSelection = getComputerChoice();
 
 
-console.log(getComputerChoice());
-
-console.log(playRound(humanSelection, computerSelection));
+//console.log(getComputerChoice());
+playGame();
