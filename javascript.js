abdfae5549d5069de6f6e3aae461e8ec19fb4422
Awardz <91509219+Awardz@users.noreply.github.com>
2024-08-11 14:22:02 -0400

    Completed Steps 1-5 in the project.  That entailed completing the getHumanChoice function, created global variables, and completing the playRound function as well as creating constant variables.

--- javascript.js
+++ javascript.js
@@ -7,11 +7,74 @@ function getComputerChoice()
     else if(num === 1)
         return "Paper"
     else
-        return "Scissors";
-    
-    
+        return "Scissors";    
 }
 
-console.log(getComputerChoice());
-//alert(getComputerChoice());
+function getHumanChoice()
+{
+    let player = prompt("Enter Rock, Paper, or Scissors");
+    return player.toLowerCase();
+}
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice)
+{
+    if(humanChoice === "rock")
+    {
+        if(computerChoice === "Paper")
+            {
+                console.log("You Lose!  Paper Wins")
+                computerScore++;
+            }  
+        if(computerChoice === "Scissors")
+            {
+                console.log("You Win! Rock Beats Scissors");
+                humanScore++;
+            }
+        else
+            console.log("Draw!")
+    }
+
+    if(humanChoice === "paper")
+        {
+            if(computerChoice === "Scissors")
+                {
+                    console.log("You Lose! Scissors Wins!");
+                    computerScore++;
+                } 
+            if(computerChoice === "Rock")
+                {
+                    console.log("You Win!  Paper Wins!");
+                    humanScore++
+                }
+            else
+                console.log("Draw!")
+        }
+
+        if(humanChoice === "scissors")
+            {
+                if(computerChoice === "Rock")
+                    {
+                        console.log("You Lose! Rock Wins!");
+                        computerScore++;
+                    }          
+                if(computerChoice === "Paper")
+                    {
+                        console.log("You Win! Scissors Wins!")
+                        humanScore++;
+                    } 
+                else
+                    console.log("Draw!")
+            }
+
+    
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+console.log(getComputerChoice());
+
+console.log(playRound(humanSelection, computerSelection));
